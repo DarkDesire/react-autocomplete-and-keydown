28f6a3797b10e5dead03d924f0f6a661cfb2a0c6
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-08 19:10:21 +0300

    Suggestions And KeyDown apps

---
+++ src/KeyDownApp.js
@@ -0,0 +1,12 @@
+import './App.css';
+import KeyDownFunctional from './KeyDownFunctional';
+
+function App() {
+  return (
+    <div className="container">
+      <KeyDownFunctional />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SuggestionsApp from './SuggestionsApp';
+import KeyDownApp from './KeyDownApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KeyDownApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
